7f0fd07fabdd1c52bf3fa21d4846a2facb230a63
mohammad hajimohammadi <hajimohammadi@amnpardaz.com>
2023-09-18 16:01:07 +0330

    read value from specified dll file

---
+++ DLLReader/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.Loader;
+
+namespace PathInputApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a path:");
+            string path = Console.ReadLine();
+
+            // read bytes for handling locking file
+            var assemblyBytes = File.ReadAllBytes(path);
+
+            var context = new AssemblyLoadContext(null, isCollectible: true);
+            var assembly = context.LoadFromStream(new MemoryStream(assemblyBytes));
+
+            var types = assembly.GetTypes();
+            var dllFileType = assembly.GetType("produce.dll.ProduceDll"); 
+
+            if (dllFileType != null)
+            {
+                var instance = Activator.CreateInstance(dllFileType);
+                var firstVal = dllFileType.GetProperty("firstvalue");
+                var timeInfo = dllFileType.GetProperty("firstvalueTime");
+                if (instance != null && firstVal != null && timeInfo != null)
+                {
+                    if (firstVal.GetValue(instance) is string firstValue &&
+                        timeInfo.GetValue(instance) is DateTimeOffset timeInfoValue)
+                    {
+                        Console.WriteLine("the value in dll file is: {0}", firstValue);
+                        Console.WriteLine("the tim value in dll file is: {0}", timeInfoValue);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Properties not found or accessible in the DLL file.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No type found in the DLL.");
+            }
+
+            // dispose DLL file
+            context.Unload();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # DLLReader
-How to use dotnet to read value from dll 
+How to use dotnet to read value from dll :
+
+consider we have a dll file , and we access the code of creating that dll , something like here :
+
+// Ignore Spelling: Vishkav
+
+namespace produce.dll;
+
+
+public class ProduceDll
+{
+    public string firstvalue { get; set; } = "this is first value that is read from dll file";
+    public DateTimeOffset firstvalueTime { get; set; } = new DateTimeOffset(2023, 2, 2, 2, 2, 2, TimeSpan.Zero);
+
+}
+
+----------------------------------------------------
+
+now you can see my code and use it to read value of this dll file
+
+good lock
